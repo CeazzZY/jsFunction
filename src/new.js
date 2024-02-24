459c28abfed57b76db93755da1fa2276e6c552e7
CeazzZY <1093918683@qq.com>
2024-02-24 22:06:10 +0800

    feat: new

---
+++ src/new.js
@@ -0,0 +1,6 @@
+function _new(constructor, ...args) {
+  let newObj = Object.create(constructor.prototype);
+  let res = constructor.apply(newObj, args);
+
+  return typeof res === "object" ? res : newObj;
+}
